1359f20ed1673a7f3266c6485b26484697477abb
c.bushehry <c.bushehry@gmail.com>
2022-11-12 12:33:46 -0800

    clicking button now shows in display section

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,9 +4,9 @@ let buttons = Array.from(document.getElementsByClassName('button'));
 
 buttons.map( button => {
     button.addEventListener('click', (e) => {
-        console.log ('clicked');
-        console.log(e);
-        console.log(e.target);
-        console.log(e.target.innerText);
+        switch(e.target.innerText){
+            default:
+                display.innerText += e.target.innerText;
+        }
     })
 });
